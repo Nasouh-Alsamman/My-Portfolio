254870931cc5bbdcc53b0929f9697d6f41abc21c
NasouhAlsamman <nasouhe@gmail.com>
2023-02-08 20:32:25 +0300

    first commit

---
+++ frontend/src/components/SocialMedia.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {BsTwitter,BsInstagram} from 'react-icons/bs'
+import {FaFacebookF} from 'react-icons/fa'
+import {AiFillLinkedin,AiFillGithub} from 'react-icons/ai'
+
+
+const SocialMedia = () => {
+  return (
+    <div className='app__social'>
+ 
+    </div>
+  )
+}
+
+export default SocialMedia
